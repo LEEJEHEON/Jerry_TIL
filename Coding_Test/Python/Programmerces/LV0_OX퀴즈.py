43318469208fc003e2549beca65ffa388ed6f74c
LEEJEHEON <dlwpgjs0723@gmail.com>
2025-10-18 19:15:05 +0900

    10/18 OX 퀴즈

---
+++ Coding_Test/Python/Programmerces/LV0_OX퀴즈.py
@@ -0,0 +1,18 @@
+# OX 퀴즈 
+
+def solution(quiz):
+    answer = []
+    
+    for q in quiz:
+        q = q.split(" ")
+        if q[1] == "-":
+            if int(int(q[0]) - int(q[2])) == int(q[4]):
+                answer.append("O")
+            else :
+                answer.append("X")
+        else :
+            if int(q[0]) + int(q[2]) == int(q[4]):
+                answer.append("O")
+            else :
+                answer.append("X")
+    return answer
